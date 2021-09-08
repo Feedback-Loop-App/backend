17a92bac3958a75f2aebf7d0ef266962b8ee439d
dunnoconnor <moconnor8@gmail.com>
2021-09-08 16:30:08 -0400

    added near-matches for username search

--- controllers/filter.js
+++ controllers/filter.js
@@ -3,20 +3,17 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post-model');
 
-
- 
 router.get("/", async (req, res) => {
 
   const searchTerm = req.query.search;
     Post.find( { $or:[ 
-      {'username':searchTerm}, 
+      {'username':{ "$regex": searchTerm, "$options": "i" }}, 
       {'tags': {$in: [searchTerm]}}, 
       { "body": { "$regex": searchTerm, "$options": "i" }}
-    
     ]}) 
     .then((posts) => res.json(posts))
     
-  });
+});
 
     
-    module.exports = router;
+module.exports = router;
