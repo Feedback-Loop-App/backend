8d712969c43aa1c50a250a207917dbd1b167cc51
dunnoconnor <moconnor8@gmail.com>
2021-09-09 10:52:05 -0400

    comments throughout

--- controllers/filter.js
+++ controllers/filter.js
@@ -1,21 +1,25 @@
 //import express and instantiate express router
 const express = require('express');
 const router = express.Router();
+
+//import post model
 const Post = require('../models/post-model');
 
-router.get("/", async (req, res) => {
-
+router.get("/", (req, res) => {
+  //get search term from query and look for regex containing search term
   const searchTerm = req.query.search;
   const closeMatch = { "$regex": searchTerm, "$options": "i" };
-    Post.find( { $or:[ 
-      {'username':closeMatch},
-      {'title':closeMatch}, 
-      {'tags':{$in: [searchTerm]}}, 
-      { "body":closeMatch}
-    ]}) 
-    .then((posts) => res.json(posts))
-    
-});
+  //find close matches for search term in username, title, tags, or body
+  Post.find( { $or:[ 
+    {'username':closeMatch},
+    {'title':closeMatch}, 
+    {'tags':{$in: [searchTerm]}}, 
+    { "body":closeMatch}
+  ]})
+  //send matching posts back to the client as json
+  .then((posts) => res.json(posts))
+  .catch(console.error);
 
+});
     
 module.exports = router;
